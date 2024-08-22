4c7bf5efc3df2a7c0d1767a8c59481e0658739d4
Paola <montserrat_osorio@yahoo.com>
2024-08-22 11:25:51 -0600

    Ejercicio strings comenzado

---
+++ A_Petya_and_Strings.cpp
@@ -0,0 +1,33 @@
+// _Paola Montserrat Osorio Garcia_
+// https://codeforces.com/contest/112/problem/A
+
+#pragma GCC optimize("Ofast,unroll-loops")//-0.0+0.0=-0
+#pragma GCC target("avx2")
+#include <bits/stdc++.h>
+
+using ull = unsigned long long;
+using ll = long long;
+using namespace std;
+#define endl '\n'
+#define dbg(...) cerr<<"LINE("<<__LINE__<<")->["<<#__VA_ARGS__<<"]: ["<<(__VA_ARGS__)<<"]\n";
+#define pb push_back
+#define F first
+#define S second
+
+int main(){
+ios::sync_with_stdio(0);cin.tie(0);
+//freopen("in.txt", "r", stdin);
+    int tc=1;
+    //cin>>tc;
+
+    string primera_string, segunda_string;
+    int comparacion;
+
+    while(tc--){
+        cin>>primera_string>>segunda_string;
+        tolower(primera_string[1]);
+        tolower(segunda_string[1]);
+
+    }
+return 0;
+}
