f184653f71ed9e49396ced2b054250db6ec9fdfd
Paola <montserrat_osorio@yahoo.com>
2024-08-27 08:14:43 -0600

    Ejercicio ruleta de letras no terminado

---
+++ A_Night_at_the_Museum.cpp
@@ -0,0 +1,48 @@
+// _Paola Montserrat Osorio Garcia_
+// https://codeforces.com/contest/731/problem/A
+
+#pragma GCC optimize("Ofast,unroll-loops")//-0.0+0.0=-0
+#pragma GCC target("avx2")
+#include <bits/stdc++.h>
+
+using ull = unsigned long long;
+using ll = long long;
+using namespace std;
+#define endl '\n'
+#define dbg(...) cerr<<"LINE("<<__LINE__<<")->["<<#__VA_ARGS__<<"]: ["<<(__VA_ARGS__)<<"]\n";
+#define pb push_back
+#define F first
+#define S second
+
+int main(){
+ios::sync_with_stdio(0);cin.tie(0);
+//freopen("in.txt", "r", stdin);
+    int tc=1;
+    //cin>>tc;
+
+    string palabra;
+    int vueltas=0, posicion=0, indice=0;
+    string letras = "abcdefghijklmnopqrstuvwxyz";//25
+
+    while(tc--){
+        cin>>palabra;
+        for (int i = 0; i < palabra.size(); i++){
+            indice = letras.find(palabra[i]);
+            cout<<endl;
+
+            cout<<indice<<" ";
+
+            if ( abs(24-indice) < indice ){
+                indice = abs(24-indice);
+            }
+
+            posicion += indice;
+
+            cout<<posicion<<" ";
+
+            vueltas += posicion;
+        }
+        cout<<endl<<vueltas;
+    }
+return 0;
+}
